2aae702cf408a884a9420c557da5e062e916fb2e
怯花颜 <45027459+XanaduAPI@users.noreply.github.com>
2021-01-22 19:34:40 -0600

    Revise some of the instructions

--- config/FindXanaduJson.cmake
+++ config/FindXanaduJson.cmake
@@ -1,32 +1,32 @@
-# 辅助输出信息
+# Explain
 message("using FindXanaduJson.cmake find")
 
-# 将头文件路径赋值给 XANADU_JSON_INCLUDE
+# Find XANADU_JSON_INCLUDE
 if (WIN32)
-	FIND_PATH(XANADU_JSON_INCLUDE XanaduJson/ C:/Xanadu/include/)
+	find_path(XANADU_JSON_INCLUDE XanaduJson/ C:/Xanadu/include/)
 elseif (MINGW)
-	FIND_PATH(XANADU_JSON_INCLUDE XanaduJson/ /usr/include/)
+	find_path(XANADU_JSON_INCLUDE XanaduJson/ /usr/include/)
 elseif (APPLE)
-	FIND_PATH(XANADU_JSON_INCLUDE XanaduJson/ /usr/local/include/)
+	find_path(XANADU_JSON_INCLUDE XanaduJson/ /usr/local/include/)
 elseif (UNIX)
-	FIND_PATH(XANADU_JSON_INCLUDE XanaduJson/ /usr/include/)
+	find_path(XANADU_JSON_INCLUDE XanaduJson/ /usr/include/)
 endif ()
 
-# 将库文件路径赋值给 XANADU_JSON_LIBRARY
+# Find XANADU_JSON_LIBRARY
 if (WIN32)
-	FIND_PATH(XANADU_JSON_LIBRARY XanaduJson.lib C:/Xanadu/lib/)
+	find_path(XANADU_JSON_LIBRARY XanaduJson.lib C:/Xanadu/lib/)
 elseif (MINGW)
-	FIND_PATH(XANADU_JSON_LIBRARY XanaduJson.dll.a /usr/lib/)
+	find_path(XANADU_JSON_LIBRARY XanaduJson.dll.a /usr/lib/)
 elseif (APPLE)
-	FIND_PATH(XANADU_JSON_LIBRARY libXanaduJson.dylib /usr/local/lib/)
+	find_path(XANADU_JSON_LIBRARY libXanaduJson.dylib /usr/local/lib/)
 elseif (UNIX)
-	FIND_PATH(XANADU_JSON_LIBRARY libXanaduJson.so /usr/lib/)
+	find_path(XANADU_JSON_LIBRARY libXanaduJson.so /usr/lib/)
 endif ()
 
 message("XANADU_JSON_INCLUDE: ${XANADU_JSON_INCLUDE}")
 message("XANADU_JSON_LIBRARY: ${XANADU_JSON_LIBRARY}")
 
+# Setting
 if(XANADU_JSON_INCLUDE AND XANADU_JSON_LIBRARY)
-	# 设置变量结果
 	set(XANADU_JSON_FOUND TRUE)
-endif(XANADU_JSON_INCLUDE AND XANADU_JSON_LIBRARY)
+endif()
